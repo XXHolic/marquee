b6947679dfac79f98c2fd7b039c32bc5330d4bca
thy <972143891@qq.com>
2020-09-20 15:39:59 +0800

    feat: add file template

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function component() {
-  var element = document.createElement("div");
-  element.innerHTML = 'fdsaf';
-  return element;
-}
-
-let element = component();
-document.body.appendChild(element);
